17634646743198a48a45e25f2b330ca305cb94f4
Fabio <fabio.andrad150@gmail.com>
2023-12-17 20:07:34 -0300

    finish

--- server/src/infra/app.ts
+++ server/src/infra/app.ts
@@ -1,7 +1,23 @@
 import express from 'express'
+import cors from 'cors'
 
 import { router } from './routes'
 
 export const app = express()
+
+const allowedOrigins = ['http://localhost:3000']
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const message =
+          'The CORS policy for this site does not allow access from the specified origin'
+        return callback(new Error(message), false)
+      }
+      return callback(null, true)
+    },
+  }),
+)
 
 app.use('/api', router)
